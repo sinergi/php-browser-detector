0906a109939cd185387ec2b8c7af4d232aec84d8
Gabriel Bull <me@gabrielbull.com>
2014-03-09 09:02:41 -0400

    Added vendor directory autoloading to unit tests bootstrap

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,6 +1,17 @@
 <?php
 
-namespace Browser;
+$vendor = realpath(__DIR__ . '/../vendor');
+
+if (file_exists($vendor . "/autoload.php")) {
+    require $vendor . "/autoload.php";
+} else {
+    $vendor = realpath(__DIR__ . '/../../../');
+    if (file_exists($vendor . "/autoload.php")) {
+        require $vendor . "/autoload.php";
+    } else {
+        throw new Exception("Unable to load dependencies");
+    }
+}
 
 require_once __DIR__.'/../src/Browser/Autoloader.php';
-Autoloader::register();
+Browser\Autoloader::register();
