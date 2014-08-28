33389abe4ef30be36d30651b5755756daaeba874
Gabriel Bull <me@gabrielbull.com>
2014-08-28 18:37:26 -0400

    Removed autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-date_default_timezone_set('UTC');
 
 $vendor = realpath(__DIR__ . '/../vendor');
 
@@ -14,20 +13,4 @@ if (file_exists($vendor . "/autoload.php")) {
     }
 }
 
-spl_autoload_register(function ($class) {
-    $class = ltrim($class, '\\');
-    $prefix = 'Browser\\Tests';
-    if (strpos($class, $prefix) === 0) {
-        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-        $class = 'Browser' . DIRECTORY_SEPARATOR . 'Tests' . DIRECTORY_SEPARATOR . '_includes' . substr($class, strlen($prefix));
-        $file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-        }
-    }
-});
-
 define('FILES', __DIR__ . "/Browser/Tests/_files");
-
-require_once __DIR__ . "/../src/Browser/Autoloader.php";
-Browser\Autoloader::register();
