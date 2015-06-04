244c88c5fb9ef243defc8ffec60914570c0bec75
Илья Гусев <mail@igusev.ru>
2015-06-04 23:05:05 +0300

    up to 100% test coverage of \Browser\Language

--- tests/Browser/Tests/LanguageTest.php
+++ tests/Browser/Tests/LanguageTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Browser\Tests;
 
+use Browser\AcceptLanguage;
 use Browser\Language;
 use PHPUnit_Framework_TestCase;
 
@@ -31,4 +32,29 @@ class LanguageTest extends PHPUnit_Framework_TestCase
     {
         $this->assertEquals('fr-CA', $this->language->getLanguageLocale());
     }
+
+    public function testConstructor()
+    {
+        $acceptLanguage = new AcceptLanguage('my_accept_language_string');
+        $language = new Language($acceptLanguage);
+
+        $this->assertInstanceOf('\Browser\AcceptLanguage', $acceptLanguage);
+        $this->assertInstanceOf('\Browser\Language', $language);
+    }
+
+    /**
+     * @expectedException \Browser\InvalidArgumentException
+     */
+    public function testConstructorException()
+    {
+        $language = new Language(1);
+    }
+
+    public function testGetLanguageLocale()
+    {
+        $language = new Language('ru,en-us;q=0.5,en;q=0.3');
+        $this->assertEquals('ru', $language->getLanguageLocale());
+    }
+
+
 }
