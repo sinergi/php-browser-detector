da79d833d64290a3996958492b2e0fc6cf12b90c
Gabriel Bull <me@gabrielbull.com>
2017-04-06 09:46:51 -0400

    Added boilerplate tests for srcipted agent

---
+++ tests/BrowserDetector/Tests/ScriptedAgentTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sinergi\BrowserDetector\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class ScriptedAgentTest extends PHPUnit_Framework_TestCase
+{
+    public function testExample()
+    {
+        $this->assertNotFalse(false);
+    }
+}
